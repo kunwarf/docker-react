a76cfe640dce4b4066185750ff01334961f1472c
kunwarf <kunwar.faisal@gmail.com>
2023-11-14 15:17:48 +0500

    added travis deploy config for aws

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { unmountComponentAtNode } from 'react-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
 test('renders without crashing', () => {
   const div = document.createElement('div');
   render(<App />);
